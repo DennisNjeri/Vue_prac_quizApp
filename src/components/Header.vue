bc35a88881898032d5c2a7589f4c5f7ec56998c9
DennisNjeri <dennisnjogu95@gmail.com>
2019-10-03 13:55:24 +0300

    Complete Vue simpleapp

---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <b-nav tabs>
+            <b-nav-item disabled><b>QUIZ APP</b></b-nav-item>
+            <b-nav-item disabled>Counter:{{numCorrect}}/{{total}}</b-nav-item>
+            <b-nav-item disabled>Another Link</b-nav-item>
+        </b-nav>
+  </div>  
+</template>
+
+<script>
+export default {
+    props:[
+        'numCorrect',
+        'total'
+    ]
+}
+</script>
